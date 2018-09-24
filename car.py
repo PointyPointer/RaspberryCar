75ec0c82bd93baafbebe2de3d38c1f66462b9185
Sigurd Holmen <sigurd.holm1@gmail.com>
2018-09-24 09:40:28 +0200

    Stable version

--- car.py
+++ car.py
@@ -1,80 +1,32 @@
-# Python 2
-
-# Own module to handle the connection
 import network_controller
 from gpiozero import CamJamKitRobot
 
 robot = CamJamKitRobot()
 
-# Max speed 1, 0 is stop
-def normalize_input(d):
-	for k in d:
-		
-		if d[k] > 1:
-			d[k] = 1
-		
-		elif d[k] < -1:
-			d[k] = -1
-	
-	return d
-
 running = True
-movment = {'x':0,'y':0}
-inc = 0.35
 
 while running:
 	inp = network_controller.get_input()
 
 	if inp == 'w':
-		movment['y'] += inc
+		robot.forward()
 	if inp == 'a':
-		movment['x'] -= inc
+
+		robot.left(speed=0.5)
 	if inp == 'd':
-		movment['x'] += inc
+		robot.right(speed=0.5)
 	if inp == 's':
-		movment['y'] -= inc
-
-	# Break down
-	if inp == 'b' :
-		movment = {'x':0,'y':0}
-		robot.stop()
-
-	# If value somehow gets over limit, reduce it to limit
-	movment = normalize_input(movment)
-
-	# Robot loses grip if turn is more then 50%
-	# Focus more on turn
-	if abs(movment['x']) > 0.5 or movment['y'] == 0:
-		if movment['x'] > 0:
-			robot.right(speed=movment['x'])
-		elif movment['x'] < -0:
-			robot.left(speed=abs(movment['x']))
-
-	else:
-		# Sends the robot forwards
-		if movment['y'] > 0:
-			if movment['x'] > 0:
-				robot.forward( speed=movment['y'], curve_right=movment['x'] )
-			else:
-				robot.forward( speed=movment['y'], curve_left=abs(movment['x']) )
-		
-		# Sending robot backwards
-		elif movment['y'] < 0:
-			if movment['x'] > 0:
-				robot.backward( speed=abs(movment['y']), curve_right=movment['x'] )
-			else:
-				robot.backward( speed=abs(movment['y']), curve_left=abs(movment['x']) )
-
-		# Make sure robot stops if no speed
+		speed_left, speed_right = robot.value
+		if speed_left == 0 and speed_right == 0:
+			robot.backward()
 		else:
 			robot.stop()
 
 	# Input is '' if connection stops
 	if inp == 'c' or inp == '':
-		print('Stopping car')
-		movment = {'x':0,'y':0}
+		robot.stop()
+		print("Turning off robot")
 		running = False
-
 	speed_left, speed_right = robot.value
 	
 	print 'Motor speed: Left:{} , Right:{}'.format( speed_left , speed_right )
--- user_getch.py
+++ user_getch.py
@@ -1,5 +1,8 @@
-# Python 2
+# Python 3
 import socket
+
+#Used to get a single char as input
+from getch import getch
 
 HOST = 'x.x.x.x'  	# Address to the raspberry pi
 PORT = 55555       	# Port to connect to (port opened on the pi)
@@ -9,10 +12,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # This fails if raspberry is not running host program
 s.connect((HOST, PORT))
 
-print 'Use <wasd> to controll the car, b to break. c to stop(remember to hit enter to send)'
+print('Use <wasd> to controll the car, b to break. c to stop(remember to hit enter to send)')
 while True:
-	inp = raw_input()
-	s.send(inp) # send command over the socket
+	inp = getch()
+	s.send(inp.encode()) # send command over the socket
 
 	if 'c' in inp:
 		print('Stopping car')
